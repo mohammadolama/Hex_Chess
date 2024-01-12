01638116661c3c06e8d1f2e0f21d562b071e6adb
Alireza Tofighi Mohammadi <alirtofighim@gmail.com>
2024-01-12 23:48:11 +0330

    add todos

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,19 @@ public class Main {
     public static void main(String[] args) {
         new Application();
 
+
+        // app.setCellProperties('f', 11, SARBAZE_SIAH, RED);
+        // app.setBottomMessage("نوبت سیاه!")
+        // app.setRemovedMohreList(....) // optional
+        // boolean app.showPopupMessage("")
+        // string app.getPromoteMohreType()
+
+        // eventListener.onCellClicked('f', 11)
+        // eventListener.onClickOutside() // نظر سادات: بیخیال
+        // eventListener.onGameLoad(File f)
+        // eventListener.onGameSave(File f)
+        // TODO: promotion
+
+
     }
 }
